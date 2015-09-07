c0be6dda61068f7de8d005871b7c8a0c7f40366c
Dênison Knob <dmknob@users.noreply.github.com>
2015-09-06 21:57:27 -0300

    Criado pithread.c

---
+++ pithread/src/pithread.c
@@ -0,0 +1,33 @@
+#include "../include/pithread.h"
+#include "../include/pidata.h"
+
+int picreate (int credCreate, void* (*start)(void*), void *arg)
+{
+  return -1;
+}
+int piyield(void)
+{
+  return -1;
+}
+int piwait(int tid)
+{
+  return -1;
+}
+int pimutex_init(pimutex_t *mtx)
+{
+  return -1;
+}
+int pilock (pimutex_t *mtx)
+{
+  return -1;
+}
+int piunlock (pimutex_t *mtx)
+{
+  return -1;
+}
+
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
