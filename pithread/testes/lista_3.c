f02d7bf488a48a64f55023c6ca4458e3b737bd08
dmknob <dmknob@gmail.com>
2015-09-26 19:04:50 -0300

    Lista ok

--- pithread/testes/lista_3.c
+++ pithread/testes/lista_3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "../include/pidata.h"
 
-#define INIT_SIZE 5
+#define INIT_SIZE 1
 #define MAX_PRIORITY 100 
 
 TCB_t *delete_element(TCB_t *first, TCB_t *deleted)
@@ -19,7 +19,10 @@ TCB_t *delete_element(TCB_t *first, TCB_t *deleted)
     else
     {
       first = deleted->next;
-      first->prev = NULL;
+      if(first != NULL)
+      {
+	first->prev = NULL;
+      }
     }
     free(deleted);
     return first;
@@ -33,6 +36,12 @@ TCB_t *new_element(TCB_t *element, int new_tid, int new_credCreate)
     new->credCreate = new_credCreate;
     new->credReal = new_credCreate;
     
+    if(element == NULL)
+    {
+      new->prev = NULL;
+      new->next = NULL;
+      return new;
+    }
     if(element->credCreate>=new_credCreate)
     {
       new->prev = element;
@@ -62,6 +71,25 @@ TCB_t *find_element(TCB_t *first, int credReal)
     first = first->next;
   }while ((aux->next != NULL)&&(aux->next->credReal >= credReal));
   return aux;
+}
+
+TCB_t *find_tid(TCB_t *first, int tid)
+{
+  if(first != NULL)
+  {
+    do
+    {
+      if(first->tid == tid)
+      {
+	return first;
+      }
+      else
+      {
+	first=first->next;
+      }
+    }while (first->next != NULL);
+  }
+  return NULL;
 }
 
 TCB_t *move_element(TCB_t *first, TCB_t *element, int credReal)
@@ -113,17 +141,24 @@ TCB_t *move_element(TCB_t *first, TCB_t *element, int credReal)
 TCB_t *insert_element(TCB_t *first, int credCreate, int tid)
 {
   TCB_t *found,*new;
-  found = find_element(first,credCreate);
+  if(first != NULL)
+  {
+    found = find_element(first,credCreate);
+  }
+  else
+  {
+    found = NULL;
+  }
   new = new_element(found,tid,credCreate);
   
-  if((found == first)&&(credCreate > found->credCreate))
+  if((found == NULL)||((found == first)&&(credCreate > found->credCreate)))
   {
       first = new;
   }
   return first;
 }
 
-/*void print_element(TCB_t *element)
+void print_element(TCB_t *element)
 {
   printf("---------------------------------------------\n");
   printf("element->tid        = %d    \n",element->tid);
@@ -131,39 +166,45 @@ TCB_t *insert_element(TCB_t *first, int credCreate, int tid)
   printf("&element            = 0x%08x\n",element);
   printf("element->prev       = 0x%08x\n",element->prev);
   printf("element->next       = 0x%08x\n",element->next);
-}*/
-
+}
 
 void print_list(TCB_t *first)
 {
   TCB_t *aux;
-  do
+  printf("=============================================================================\n");
+  if(first != NULL)
   {
-    aux=first;
-    printf("---------------------------------------------\n");
-    printf("list->tid        = %d    \n",first->tid);
-    printf("list->credCreate = %d    \n",first->credCreate);
-    printf("list->credReal   = %d    \n",first->credReal);
-    //printf("&list          = 0x%08x\n",first);
-    if(first->prev!=NULL)
+    do
     {
-      printf("list->prev       = %d\n",first->prev->tid);//0x%08x\n",first->prev);
-    }
-    if(first->next!=NULL)
-    {
-      printf("list->next       = %d\n",first->next->tid);//0x%08x\n",first->next);
-    }
-    first=first->next;
-  }while (aux->next != NULL);
+      aux=first;
+      printf("---------------------------------------------\n");
+      printf("list->tid        = %d    \n",first->tid);
+      printf("list->credCreate = %d    \n",first->credCreate);
+      printf("list->credReal   = %d    \n",first->credReal);
+      //printf("&list          = 0x%08x\n",first);
+      if(first->prev!=NULL)
+      {
+	printf("list->prev       = %d\n",first->prev->tid);//0x%08x\n",first->prev);
+      }
+      if(first->next!=NULL)
+      {
+	printf("list->next       = %d\n",first->next->tid);//0x%08x\n",first->next);
+      }
+      first=first->next;
+    }while (aux->next != NULL);
+  }
 }
 
 
 int main()
 {
-  int tid;
-  TCB_t *list;
+  int tid=0;
+  TCB_t *first;
+  
+  first = NULL;
   //TCB_t *credCreate[MAX_PRIORITY+1];
   
+  /*
   list = malloc(INIT_SIZE * sizeof(TCB_t));
   
   for(tid=0;tid<INIT_SIZE;tid++)
@@ -186,31 +227,29 @@ int main()
       list[tid].prev = &list[tid-1];
       list[tid].next = &list[tid+1];
     } 
-  }
+  }*/
    
-  list = insert_element(list,102,tid++);
-  list = insert_element(list,99,tid++);
-  list = insert_element(list,99,tid++);
-  list = insert_element(list,99,tid++);
+  first = insert_element(first,100,tid++);
+  first = insert_element(first,90,tid++);
+  first = insert_element(first,80,tid++);
+  first = insert_element(first,70,tid++); 
   
-  //list = delete_element(list,find_element(list,95));
+  print_list(first);
   
-  list = move_element(list,find_element(list,70),101);
+  first = delete_element(first,find_element(first,100));
+  first = delete_element(first,find_element(first,90));
+  first = delete_element(first,find_element(first,80));
+  first = delete_element(first,find_element(first,70));
+  //print_element(first);
+  print_list(first);
   
-  //insert_element(list,100,&tid);
-  //insert_element(list,61,&tid);
-  print_list(list);
-  //getchar();
-  //print_element(find_element(list,40));
-  //printf("sizeof(list) = %d\n",sizeof(list));
+  first = insert_element(first,90,tid++);
+  first = insert_element(first,90,tid++);
+  first = insert_element(first,90,tid++);
+  first = insert_element(first,90,tid++);
+  first = insert_element(first,90,tid++);
+  first = delete_element(first,find_tid(first,6));
+  print_list(first);
   
-  //free(list);
-  //clean_list(list);
   return(0);
 }
-
-//OK-Pegar o tempo pelo programa
-//OK-Mudar para calcular os n primeiros primos
-//OK-Salvar os primos numa matrix, e depois exibi-los
-//  -Comparar desempenho int contra float
-//  -Otimizar...
